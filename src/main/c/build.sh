de0a604595040dfacab58e013c813f889c819112
Brock Noland <brock@cloudera.com>
2011-09-02 18:51:34 -0500

    Fix build script

--- src/main/c/build.sh
+++ src/main/c/build.sh
@@ -6,4 +6,4 @@ then
     CFLAGS="$CFLAGS -I$JAVA_HOME/include/linux"
 fi
 SRC="com_cloudera_training_hadoop_image_NativeFunctions.c"
-bash -x -c "gcc $SRC -Wall -fPIC -shared -Wl,-soname,$LIB -lWand -lMagick -o $LIB" || exit $?
+bash -x -c "gcc $SRC -Wall -fPIC $CFLAGS -shared -Wl,-soname,$LIB -lWand -lMagick -o $LIB" || exit $?
